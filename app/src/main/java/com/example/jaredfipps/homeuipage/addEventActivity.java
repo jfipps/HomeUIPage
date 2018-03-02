5247b136bf0d677cc3758d810ca61404c4334efb
jfipps <jfipps1995@csu.fullerton.edu>
2018-03-01 20:42:14 -0800

    Main page push

---
+++ app/src/main/java/com/example/jaredfipps/homeuipage/addEventActivity.java
@@ -0,0 +1,59 @@
+package com.example.jaredfipps.homeuipage;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+
+public class addEventActivity extends AppCompatActivity {
+
+    public EventClass event = new EventClass();
+    public ArrayList<EventClass> eventList = new ArrayList<EventClass>();
+
+    public String checkFlag;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_event);
+        Intent intent = getIntent();
+        eventList = (ArrayList<EventClass>) intent.getSerializableExtra("events");
+    }
+
+    public void addEvent (View v) {
+        EditText title = findViewById(R.id.title);
+        event.setEventTitle(title.getText().toString());
+
+        EditText desc = findViewById(R.id.description);
+        event.setDesc(desc.getText().toString());
+
+        EditText hourText = findViewById(R.id.hour);
+        String hourString = hourText.getText().toString();
+        int hourInt = Integer.parseInt(hourString);
+        event.setHour(hourInt);
+
+        EditText minText = findViewById(R.id.minute);
+        String minString = minText.getText().toString();
+        int minInt = Integer.parseInt(minString);
+        event.setMinute(minInt);
+
+        eventList.add(event);
+
+        checkFlag = "addEvent";
+
+        Toast.makeText(getApplicationContext(), "Event Added", Toast.LENGTH_SHORT);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("flag", checkFlag);
+        intent.putExtra("eventUpdate", eventList);
+        startActivity(intent);
+    }
+}
